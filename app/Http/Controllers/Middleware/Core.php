f502a9718e89eeb90b1e07e146fca042f33ebf66
Md Rasel Mia <62537005+mdraselmia2018@users.noreply.github.com>
2024-10-24 19:40:43 +0600

    cros polic

--- app/Http/Controllers/Middleware/Core.php
+++ app/Http/Controllers/Middleware/Core.php
@@ -15,13 +15,20 @@ class Cors
      * @return \Illuminate\Http\Response
      */
     public function handle(Request $request, Closure $next)
-    {
-        $response = $next($request);
-
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
-
-        return $response;
+{
+    if ($request->getMethod() === "OPTIONS") {
+        return response('', 204)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
     }
+
+    $response = $next($request);
+
+    $response->headers->set('Access-Control-Allow-Origin', '*');
+    $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+    $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+
+    return $response;
+}
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\Cors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->pushMiddleware(Cors::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
