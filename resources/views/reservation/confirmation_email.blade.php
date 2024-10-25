636f8f66f8e51cad8f59563d37cc0631bba0aba4
Md Rasel Mia <62537005+mdraselmia2018@users.noreply.github.com>
2024-10-25 17:02:27 +0600

    email fixed

--- resources/views/reservation/confirmation_email.blade.php
+++ resources/views/reservation/confirmation_email.blade.php
@@ -111,7 +111,6 @@
             <h4>Reservation ID: {{ $reservation->reservation_id }}.</h4>
             <p>Dear <strong>{{ ucwords($reservation->guest_information->first_name) }} {{ ucwords($reservation->guest_information->last_name) }}</strong>,</p>
             <p>If you need to make any changes to your reservation, please call <strong>{{ $reservation->restaurant->phone }}</strong>.</p>
-            <p>Here’s a reminder of your reservation and details you gave us. Please take a moment to check everything is in order.</p>
             <p><em>Need help?</em> If there is anything else you would like to know or amend, please call <strong>{{ $reservation->restaurant->phone }}</strong>.</p>
             <div class="reservation-details">
                 <table width="100%" style="color: white;">
