0692ab6971ee4420d682cdd64d58a9252ea61437
Md Rasel Mia <62537005+mdraselmia2018@users.noreply.github.com>
2025-01-30 20:18:50 +0600

    email tamplete redesign

--- resources/views/email_template/enquiry.blade.php
+++ resources/views/email_template/enquiry.blade.php
@@ -19,23 +19,30 @@
                     </tr>
                     <!-- Message Section -->
                     <tr>
-                        <td align="center" style="font-size: 18px; font-weight: bold; color: #333;">
-                            Dear <strong>{{ ucwords($data->first_name) }} {{ ucwords($data->last_name) }}</strong>, <br>
-                            Thank you for contacting with {{$data->restaurant_name}}. We will get back to you soon. <br>
-                            <em>Need help?</em> If there is anything else you would like to know or amend, please call <strong>{{ $data->phone }}</strong>.
+                        <td align="left" style="font-size: 18px; color: #333;">
+                            <strong>Dear {{ ucwords($data->first_name) }} {{ ucwords($data->last_name) }}</strong>, <br>
+                            Thank you for contacting with {{$data->restaurant_name}}. We will get back to you soon.
                         </td>
                     </tr>
                     <tr>
-                        <td align="center" style="padding: 20px 0; font-size: 16px; font-weight: bold; color: #333;">
+                        <td align="left" style="padding: 20px 0; font-size: 16px; font-weight: bold; color: #333;">
                             Here are your enquiry details:
                         </td>
                     </tr>
                     <!-- Enquiry Details -->
                     <tr>
                         <td style="padding: 0 40px; font-size: 14px; color: #333;">
-                            <p>{{ ucwords($data->first_name) }} {{ ucwords($data->last_name) }}</p>
-                            <p>{{ $data->email }}</p>
-                            <p>{{ $data->phone }}</p>
+                            <p><strong>Name: </strong>{{ ucwords($data->first_name) }} {{ ucwords($data->last_name) }}</p>
+                            <p><strong>Email: </strong>{{ $data->email ? $data->email : 'N/A' }}</p>
+                            <p><strong>Phone: </strong>{{ $data->phone ? $data->phone : 'N/A' }}</p>
+                            <p><strong>Restaurant name: </strong>{{ $data->restaurant_name ? $data->restaurant_name : 'N/A' }}</p>
+                            <p><strong>Post code: </strong>{{ $data->post_code ? $data->post_code : 'N/A' }}</p>
+                            <p><strong>Message: </strong>{{ $data->message ? $data->message : 'N/A' }}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" style="font-size: 18px; font-weight: bold; color: #333;">
+                            <em>Need help?</em> If there is anything else you would like to know or amend, please call {{ $data->phone }}.
                         </td>
                     </tr>
                     <!-- Footer -->
